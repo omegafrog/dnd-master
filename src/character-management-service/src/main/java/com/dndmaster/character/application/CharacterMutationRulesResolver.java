c8d56026275da091c25f6f8173bf7700145720fe
Kim Ji Woo <jiwoo22110103@gmail.com>
2026-08-03 10:09:43 +0900

    feat(character): resolve D&D 5e 2014 mutation rules

--- src/character-management-service/src/main/java/com/dndmaster/character/application/CharacterMutationRulesResolver.java
+++ src/character-management-service/src/main/java/com/dndmaster/character/application/CharacterMutationRulesResolver.java
@@ -7,6 +7,14 @@ import com.dndmaster.character.domain.SheetEdition;
 public interface CharacterMutationRulesResolver {
     CharacterMutationRules rulesFor(SheetEdition edition);
 
+    static CharacterMutationRulesResolver standard() {
+        CharacterMutationRules dnd5e2014 = new Dnd5e2014CharacterMutationRules();
+        return edition -> switch (edition) {
+            case DND_5E_2014 -> dnd5e2014;
+            case DND_5E_2024 -> CharacterMutationRules.allowAll();
+        };
+    }
+
     static CharacterMutationRulesResolver permissive() {
         return ignored -> CharacterMutationRules.allowAll();
     }
